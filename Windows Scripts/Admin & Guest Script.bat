c183c0ebd2bb18070a4e168ba6d805c9dbf414fc
CollinJoseph212 <collin.joseph@stu.johnson.kyschools.us>
2025-12-12 16:40:35 -0500

    Update Admin & Guest Script.bat
    Fix logic for enabling Admin account in script.

--- Windows Scripts/Admin & Guest Script.bat
+++ Windows Scripts/Admin & Guest Script.bat
@@ -9,7 +9,9 @@ echo ================================
 cls
 set /p "choicea= Would you like to enable Admin account? y/n"
 if %choicea% == y net user administrator /active:yes goto guest
+if %choicea% == y goto guest
 if %choicae% == n net user administrator /active:no goto guest
+if %choicae% == n goto guest
 
 :guest
 cls
