60c7cbc671222d98d35680260ad2e5bb80ca82dd
CollinJoseph212 <collin.joseph@stu.johnson.kyschools.us>
2025-12-12 16:33:34 -0500

    Add files via upload

---
+++ Windows Scripts/Admin & Guest Script.bat
@@ -0,0 +1,18 @@
+@echo off
+title CP Admin/guest 
+
+echo ================================
+echo          Admin/guest 
+echo ================================
+
+:admin
+cls
+set /p "choicea= Would you like to enable Admin account? y/n"
+if %choicea% == y net user administrator /active:yes goto guest
+if %choicae% == n net user administrator /active:no goto guest
+
+:guest
+cls
+set /p "choiceg= Would you like to enable Guest account? y/n"
+if %choiceg% == y net user guest /active:yes 
+if %choiceg% == n net user guest /active:no
